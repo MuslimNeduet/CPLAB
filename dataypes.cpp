f1117d9c4211430cf99acf2bc41bbecd9d42e5ff
Muslimneduet <muslim.neduet@gmail.com>
2025-04-23 18:10:38 +0500

    datatypes

---
+++ dataypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// sizeof() function calculates the Bytes
+int main(void)
+{
+ cout<<"Integer Bytes="<<sizeof(int);
+ cout<<"\nDouble Bytes="<<sizeof(double);
+ cout<<"\nCharacter Bytes="<<sizeof(char);
+ cout<<"\nBoolean Bytes="<<sizeof(bool);
+ return 0;
+}
