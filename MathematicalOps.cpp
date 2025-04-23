2b3d357026fc77d619e26a6626cb2ac38b274508
Muslimneduet <muslim.neduet@gmail.com>
2025-04-23 18:13:24 +0500

    mathops

---
+++ MathematicalOps.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number1;
+int number2;
+int result;
+cout<<"Please enter number1 & number2";
+cin>>number1>>number2;
+result = number1 + number2; // addition
+cout<<result<<endl;
+result = number1 - number2; // subtraction
+cout<<result<<endl;
+result = number1 * number2; // multiplication
+cout<<result<<endl;
+result = number1 / number2; // division
+cout<<result<<endl;
+result = number1 % number2; // remainder division
+cout<<result;
+return 0;
+}
