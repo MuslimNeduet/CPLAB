653ea3bceb3c5e1f9710a55a7655d54121760cc5
Muslimneduet <muslim.neduet@gmail.com>
2025-04-23 18:14:05 +0500

    rolesofvariable

---
+++ rolesofvariables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+ int age; // declaring int variable
+ string name;// declaring string variable
+ float height_in_cms, weight_in_kg; // 2 float variables
+ age = 19; // now assigning values to variables
+ name = "Ahmed Khan";
+ height_in_cms = 123.8;
+ weight_in_kg = 58.7;
+ cout<<"Name:"<<name<<"\t Age:"<<age<<endl;
+ cout<<"Height(cm):"<<height_in_cms<<"\t Weight(kg):"<<weight_in_kg;
+ return 0;
+}
